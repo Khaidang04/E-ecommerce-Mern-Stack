2bbe7805071e79e645a5091a0d4e5ff5ebf191af
KhaiTapCode04 <2251120162@ut.edu.vn>
2025-05-22 23:38:22 +0700

    udProduct_ud_get_delete

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,8 +1,12 @@
-const { createProduct } = require("../controllers/product.controller");
+const { createProduct, updateProduct, deleteProduct, getProduct, getProducts } = require("../controllers/product.controller");
 const { verifyAdmin } = require("../middleware/verifyToken");
 const { parser } = require("../utils/cloudinary");
 const router = require('express').Router();
 
 router.post("/", verifyAdmin, parser.single("image"), createProduct);
+router.put("/:id", verifyAdmin, parser.single("image"), updateProduct);
+router.delete("/:id", verifyAdmin, deleteProduct);
+router.get("/:id", getProduct);
+router.get("/", getProducts);
 
 module.exports = router;
