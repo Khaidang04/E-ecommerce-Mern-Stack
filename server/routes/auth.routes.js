632c3cbb1cacc692fcef0b1fee9ed549d066f541
KhaiTapCode04 <2251120162@ut.edu.vn>
2025-05-26 21:35:43 +0700

    udUserFull

--- server/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { register, login } = require("../controllers/auth.controller");
-const User = require("../models/user.model");
-
-router.post("/register", register);
-router.post("/login", login);
-
-module.exports = router;
